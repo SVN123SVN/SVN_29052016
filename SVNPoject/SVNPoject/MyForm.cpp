df649b0409e87a8f669f90c5a6a591ce48cb73cc
azzret <jusuev.azret@bk.ru>
2016-05-29 22:10:14 +0300

    Написал CPP

--- SVNPoject/SVNPoject/MyForm.cpp
+++ SVNPoject/SVNPoject/MyForm.cpp
@@ -1,2 +1,11 @@
 #include "MyForm.h"
+#include<Windows.h>
+using namespace SVNPoject;
 
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	Application::EnableVisualStyle();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	return 0;
+}
